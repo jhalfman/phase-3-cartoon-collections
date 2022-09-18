69896deb7b0c4d9bb3ff92d1a21cdd2a8fb4a41a
Jordan Halfman <jordanhalfman@gmail.com>
2022-09-18 12:09:14 -0700

    commit tree 8eb1578f12e04688b3c145dab244fccac753b6bc

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array# code an argument here
   # Your code here
+  array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet elements# code an argument here
   # Your code here
+  summon_array = []
+  elements.map do |element|
+    summon_array << element.capitalize + "!"
+  end
+  summon_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls# code an argument here
   # Your code here
+  long_call = false
+  calls.each do |call|
+    if call.length > 4
+      long_call = true
+    end
+  end
+
+  long_call
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese cheese_check# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found = nil
+  cheese_types.each do |cheese|
+    if cheese_check.include? cheese
+      found = cheese
+    end
+  end
+  found
 end
